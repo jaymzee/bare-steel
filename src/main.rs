8076487d86e8e35a3529ade95dd15170620471c3
jaymzee <jaymzee@users.noreply.github.com>
2020-12-27 10:55:25 -0500

    freestanding rust binary

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // entry point
+    loop {}
+}
+
+// this function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
