2f528f78e89255e48275286962bd2ab28b31f254
jaymzee <jaymzee@users.noreply.github.com>
2021-06-26 18:16:23 -0400

    post-04 completed

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,40 @@
 
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
 use core::panic::PanicInfo;
+use blog_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("hello!");
     println!("some numbers: {} {}", 42, 1.337);
-    println!("bye");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
-// this function is called on panic
+// our existing panic handler
+#[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info);
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
