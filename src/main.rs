e6c822d958e9e2a0ba772d6f92fc39a1e69376ac
jaymzee <jaymzee@users.noreply.github.com>
2021-06-26 16:10:02 -0400

    post-03 completed

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,24 @@
 // based on https://os.phil-opp.com/minimal-rust-kernel/
 //
+
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
 use core::panic::PanicInfo;
-
-fn locate_text(row: u8, col: u8, s: &[u8]) {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in s.iter().enumerate() {
-        unsafe {
-            let dx = row as isize * 80 + col as isize + i as isize;
-            *vga_buffer.offset(2 * dx) = byte;
-            *vga_buffer.offset(2 * dx + 1) = 0xb; // set attribute
-        }
-    }
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    locate_text(0, 0, b"Hello, World!");
-    locate_text(1, 5, b"Goodbye");
+    println!("hello!");
+    println!("some numbers: {} {}", 42, 1.337);
+    println!("bye");
 
     loop {}
 }
 
 // this function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
