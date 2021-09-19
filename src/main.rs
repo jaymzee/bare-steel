88da23d614d991e0f8134a43d8828f870c220258
jaymzee <jaymzee@users.noreply.github.com>
2021-09-18 23:42:40 -0400

    minimal example using the new bootloader 0.10.8

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,23 @@
-// based on https://os.phil-opp.com/minimal-rust-kernel/
-//
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
-fn locate_text(row: u8, col: u8, s: &[u8]) {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in s.iter().enumerate() {
-        unsafe {
-            let dx = row as isize * 80 + col as isize + i as isize;
-            *vga_buffer.offset(2 * dx) = byte;
-            *vga_buffer.offset(2 * dx + 1) = 0xb; // set attribute
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    // turn the screen gray
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        for byte in framebuffer.buffer_mut() {
+            *byte = 0x90;
         }
     }
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    locate_text(0, 0, b"Hello, World!");
-    locate_text(1, 5, b"Goodbye");
 
     loop {}
 }
 
-// this function is called on panic
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
